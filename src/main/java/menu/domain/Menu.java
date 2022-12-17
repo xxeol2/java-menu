bbe5aa85fd7fe53996fb36ef638aaff36ad496cd
xxeol2 <jhj01177@naver.com>
2022-12-17 16:37:23 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -26,10 +26,6 @@ public class Menu {
         return this.name.equals(menuName);
     }
 
-    public boolean checkCategory(Category category) {
-        return this.category.equals(category);
-    }
-
     public String getName() {
         return name;
     }
