cfca08930d711570ab34aebb46d671538a64007a
xxeol2 <jhj01177@naver.com>
2022-12-17 16:43:31 +0900

    test: 코치의 이름은 중복될 수 없다.

--- src/test/java/menu/CoachTest.java
+++ src/test/java/menu/CoachTest.java
@@ -1,9 +1,12 @@
 package menu;
 
+import static menu.validator.CoachValidator.validateCoach;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import menu.domain.Coach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,5 +22,12 @@ class CoachTest {
     @ParameterizedTest
     void 코치_이름이_2글자보다_짧거나_4글자보다_길면_에러처리한다(String coachName) {
         assertThatThrownBy(() -> new Coach(coachName)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 코치_이름이_중복되면_에러처리한다() {
+        List<String> coachNames = List.of("포비", "포비", "설희");
+        assertThatThrownBy(() -> validateCoach(coachNames)).isInstanceOf(
+                IllegalArgumentException.class);
     }
 }
