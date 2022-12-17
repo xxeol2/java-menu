ebcfbd42e5bcdd25772df0189a8afa149618e86d
xxeol2 <jhj01177@naver.com>
2022-12-17 13:28:36 +0900

    refactor: Category와 Menu에 hashCode equals 오버라이드

--- src/main/java/menu/Category.java
+++ src/main/java/menu/Category.java
@@ -1,6 +1,7 @@
 package menu;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Category {
 
@@ -19,5 +20,25 @@ public class Category {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        Category that = (Category) o;
+        return name == that.name;
     }
 }
--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,5 +1,7 @@
 package menu;
 
+import java.util.Objects;
+
 public class Menu {
 
     private final String name;
@@ -15,5 +17,25 @@ public class Menu {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Menu)) {
+            return false;
+        }
+        Menu that = (Menu) o;
+        return name == that.name;
     }
 }
