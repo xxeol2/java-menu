9668317badff1bae25c084bd850128c9831cb4ac
xxeol2 <jhj01177@naver.com>
2022-12-17 13:49:21 +0900

    test: 코치의 이름은 최소 2글자, 최대 4글자이다.

---
+++ src/test/java/menu/CoachTest.java
@@ -0,0 +1,22 @@
+package menu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CoachTest {
+
+    @ValueSource(strings = {"일이", "일이삼", "일이삼사"})
+    @ParameterizedTest
+    void 코치_이름은_최소_2글자_최대_4글자이다(String coachName) {
+        assertThat(Coach.of(coachName)).isInstanceOf(Coach.class);
+    }
+
+    @ValueSource(strings = {"", "일", "일이삼사오"})
+    @ParameterizedTest
+    void 코치_이름이_2글자보다_짧거나_4글자보다_길면_에러처리한다(String coachName) {
+        assertThatThrownBy(() -> Coach.of(coachName)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
